5895bb7804f0f851ec535ea3a8ec657a5788a5dd
Phelipe Pereira <phelipescript@gmail.com>
2023-12-27 15:52:59 -0300

    CSS-in-JS - Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components/native'
-import { ActivityIndicator } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,13 +7,14 @@ import {
 
 import Groups from '@screens/Groups'
 import theme from '@theme/index'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
