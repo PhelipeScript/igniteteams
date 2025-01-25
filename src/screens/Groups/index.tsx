62a9d6a2d149481d3ff0bcfbf012e357e6965d24
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2025-01-25 18:46:00 -0300

    Navegação - Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,15 @@ import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { EmptyList } from '@components/EmptyList'
 import { Button } from '@components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -25,7 +31,7 @@ export function Groups() {
         )}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
