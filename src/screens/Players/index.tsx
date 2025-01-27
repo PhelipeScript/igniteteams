2ed9e37bede5b616316c6c6f59fabdbaf723d72d
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2025-01-26 21:18:52 -0300

    Local Storage - Listando Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,22 +4,54 @@ import { Highlight } from '@components/Highlight'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Input } from '@components/Input'
 import { Filter } from '@components/Filter'
-import { FlatList } from 'react-native'
+import { Alert, FlatList } from 'react-native'
 import { useState } from 'react'
 import { PlayerCard } from '@components/PlayerCard'
 import { EmptyList } from '@components/EmptyList'
 import { Button } from '@components/Button'
 import { useRoute } from '@react-navigation/native'
+import { AppError } from '@utils/AppError'
+import { playerAddByGroup } from '@storage/player/playerAddByGroup'
+import { playersGetByGroup } from '@storage/player/playersGetByGroup'
 
 type RouteParams = {
   group: string
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('')
   const [team, setTeam] = useState<string>('Time 1')
   const [players, setPlayers] = useState([])
   const route = useRoute()
   const { group } = route.params as RouteParams
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert(
+        'Nova pessoa',
+        'Informe o nome da pessoa para adicionar.',
+      )
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group)
+      const players = await playersGetByGroup(group)
+
+      console.log(players)
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message)
+      } else {
+        console.error(error)
+        Alert.alert('Nova pessoa', 'Não foi possível adicionar a pessoa.')
+      }
+    }
+  }
 
   return (
     <Container>
@@ -28,9 +60,13 @@ export function Players() {
       <Highlight title={group} subtitle="adicione a galera e separe os times" />
 
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <Input
+          onChangeText={setNewPlayerName}
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
 
-        <ButtonIcon icon="add" />
+        <ButtonIcon onPress={handleAddPlayer} icon="add" />
       </Form>
 
       <HeaderList>
